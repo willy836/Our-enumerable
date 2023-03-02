cca1da10415246ffc1f59a9d6f560aa5d1af84f7
gauravgangwarweb <gauravgangwar501@gmail.com>
2023-03-02 18:09:58 +0530

    Fix linter errors

--- my_list.rb
+++ my_list.rb
@@ -15,15 +15,15 @@ class MyList
 end
 
 # Create our list
-list = MyList.new(1, 2, 3, 4)
+# list = MyList.new(1, 2, 3, 4)
 
 # Test #all?
 # puts (list.all? { |e| e < 5 }) => true
 # puts (list.all? {|e| e > 5}) => false
 
-#Test #any?
+# Test #any?
 # puts (list.any? {|e| e == 2}) => true
 # puts (list.any? {|e| e == 5}) => false
 
-#Test #filter
+# Test #filter
 # puts (list.filter {|e| e.even?}) => 2, 4
